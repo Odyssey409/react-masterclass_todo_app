730b84f5758768426e94d03f82218151f8eac0cf
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-08-03 23:35:37 +0900

    section#6.6 Form Validation
    react-hook-form을 사용하여 폼을 쉽게 만드는 법을 학습함

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,27 +1,42 @@
-import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+// function ToDoList() {
+//   const [toDo, setToDo] = useState("");
+
+//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = event;
+//     setToDo(value);
+//   };
+//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//     console.log(toDo);
+//   };
+
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input
+//           onChange={onChange}
+//           value={toDo}
+//           placeholder="Write a to do"
+//         ></input>
+//         <button>Add</button>
+//       </form>
+//     </div>
+//   );
+// }
 
 function ToDoList() {
-  const [toDo, setToDo] = useState("");
-
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDo(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
-
+  const { register, watch } = useForm();
+  console.log(watch());
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          placeholder="Write a to do"
-        ></input>
+      <form>
+        <input {...register("toDo")} placeholder="Write a to do"></input>
+        <input {...register("name")} placeholder="Write your name"></input>
+        <input {...register("email")} placeholder="Write your email"></input>
         <button>Add</button>
       </form>
     </div>

969a852a90924e2ac5b6527f709acfc93c0c0ef7
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-08-06 00:36:48 +0900

    section#6.14 Immutability part One
    현재 갖고 있는 toDo목록들의 id와 클릭된 toDo목록의 id를 가져와 비교한 후 일차하는 부분의 인덱스를 가져와 어디인지 찾아내고 그 목록의 버튼 중 어느 버튼을 클릭했는지 받아와서 name을 통해 어떤 category로 이동하고 싶은지를 판단

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { IToDo, toDoState } from "./atoms";
-import { useRecoilState } from "recoil";
+import { useSetRecoilState } from "recoil";
 
 function ToDo({ text, category, id }: IToDo) {
-  const setToDos = useRecoilState(toDoState);
+  const setToDos = useSetRecoilState(toDoState);
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
     } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      //인자로 받아온 toDo의 아이디와 클릭된 투두 목록의 아이디와 일치하는 인데스를 찾는 역할
+      const oldToDo = oldToDos[targetIndex];
+      const newToDo = { text, id, category: name };
+      // name은 어떤 버튼을 눌렀는지에서 받아오는 currentTarget: { name }으로 설정해야 어떤 카테고리로 변경할 것인지 분별가능
+      return oldToDos;
+    });
   };
   return (
     <li>

5c78ecf3e274d4323243b8b3146b178ebfe5551d
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-08-05 18:40:44 +0900

    section#6.10 Recap
    기존 연습을 지우고 ToDo 만들기로 돌아와서 react-hook-form을 활용한 코드들로 재탄생 시킴

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,126 +1,23 @@
 import { useForm } from "react-hook-form";
 
-// function ToDoList() {
-//   const [toDo, setToDo] = useState("");
-
-//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-//     const {
-//       currentTarget: { value },
-//     } = event;
-//     setToDo(value);
-//   };
-//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault();
-//     console.log(toDo);
-//   };
-
-//   return (
-//     <div>
-//       <form onSubmit={onSubmit}>
-//         <input
-//           onChange={onChange}
-//           value={toDo}
-//           placeholder="Write a to do"
-//         ></input>
-//         <button>Add</button>
-//       </form>
-//     </div>
-//   );
-// }
-
 interface IForm {
   toDo?: string;
-  name: string;
-  email: string;
-  passwords: string;
-  passwordCheck: string;
-  extraError?: string;
 }
 
 function ToDoList() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setError,
-  } = useForm<IForm>({
-    defaultValues: {
-      email: "@naver.com",
-    },
-  });
-  const onValid = (data: IForm) => {
-    if (data.passwords !== data.passwordCheck) {
-      setError(
-        "passwordCheck",
-        { message: "Passwords are incorrect" },
-        { shouldFocus: true }
-      );
-    }
-    // setError("extraError", { message: "Server offLine." });
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const hadleValid = (data: IForm) => {
+    console.log("add to do", data.toDo);
+    setValue("toDo", ""); // 제출하면 입력칸 비워주는 코드
   };
   return (
     <div>
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={handleSubmit(onValid)}
-      >
-        <input {...register("toDo")} placeholder="Write a to do"></input>
+      <form onSubmit={handleSubmit(hadleValid)}>
         <input
-          {...register("name", {
-            required: "name is requried",
-            validate: {
-              noNico: (value) =>
-                value.includes("nico") ? "no nicos allowed" : true,
-              noNick: (value) =>
-                value.includes("nick") ? "no nicos allowed" : true,
-            },
-          })}
-          placeholder="Write your name"
+          {...register("toDo", { required: "Please Write a To Do" })}
+          placeholder="Write a to do"
         ></input>
-        <span style={{ color: "red" }}>{errors?.name?.message as string}</span>
-        <input
-          {...register("email", {
-            required: "email is requried",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: "Only naver.com emails allowed",
-            },
-            minLength: 10,
-          })}
-          placeholder="Write your email"
-        ></input>
-        <span style={{ color: "red" }}>{errors?.email?.message as string}</span>
-        <input
-          {...register("passwords", {
-            required: true,
-            minLength: {
-              value: 5,
-              message: "Your password is too short",
-            },
-          })}
-          placeholder="Write your password"
-        ></input>
-        <span style={{ color: "red" }}>
-          {errors?.passwords?.message as string}
-        </span>
-
-        <input
-          {...register("passwordCheck", {
-            required: true,
-            minLength: {
-              value: 5,
-              message: "Your password is too short",
-            },
-          })}
-          placeholder="Write your passwordCheck"
-        ></input>
-        <span style={{ color: "red" }}>
-          {errors?.passwordCheck?.message as string}
-        </span>
         <button>Add</button>
-        <span style={{ color: "red" }}>
-          {errors?.extraError?.message as string}
-        </span>
       </form>
     </div>
   );

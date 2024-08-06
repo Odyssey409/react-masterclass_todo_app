2ff96770532e893896155b9a2ab9a7394992e521
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-08-06 15:38:00 +0900

    section#6.16 Selectors part One
    selector를 사용하여 atom으로부터 받아온 toDoState에서 카테고리를 선택하여 배열로 반환하고 그 배열들을 사용하여 카테고리를 구분할 수 있는 기초를 만듦.

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,22 +1,42 @@
 import { useRecoilValue } from "recoil";
 import CreateToDo from "./CreateToDo";
-import { toDoState } from "./atoms";
+import { toDoSelector } from "./atoms";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
-
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <CreateToDo />
+      <h2>To Do</h2>
       <ul>
-        {toDos.map((toDo) => (
+        {toDo.map((toDo) => (
           // <ToDo text={toDo.text} category={toDo.category} id={toDo.id} />
-          <ToDo key={toDo.id} {...toDo} /> // 이 방식은 위와 같은 내용으로 작동함 ...은 안에 요쇼를 벗기는 역할을 하기 때문
+          <ToDo key={toDo.id} {...toDo} />
+          // 이 방식은 위와 같은 내용으로 작동함 ...은 안에 요쇼를 벗기는 역할을 하기 때문
         ))}
       </ul>
+      <hr />
+      <h2>Doing</h2>
+      <ul>
+        {doing.map((toDo) => (
+          // <ToDo text={toDo.text} category={toDo.category} id={toDo.id} />
+          <ToDo key={toDo.id} {...toDo} />
+          // 이 방식은 위와 같은 내용으로 작동함 ...은 안에 요쇼를 벗기는 역할을 하기 때문
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {done.map((toDo) => (
+          // <ToDo text={toDo.text} category={toDo.category} id={toDo.id} />
+          <ToDo key={toDo.id} {...toDo} />
+          // 이 방식은 위와 같은 내용으로 작동함 ...은 안에 요쇼를 벗기는 역할을 하기 때문
+        ))}
+      </ul>
+      <hr />
     </div>
   );
 }

765faee2ed4ecb515d93b7840cf3ae017bdc5d7a
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-08-06 15:57:32 +0900

    section#6.17 Selectors part Two
    categoryState를 만들어서 사용자가 선택한 카테고리에 해당하는 toDo들만 보여주는 기능을 만듦. 카테고리 선택한 value를 받아와서 어떤 카테고리인지 파악한 후 toDoSelector에서 해당하는 카테고리에 있는 toDo들만 반환하도록 설계

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,42 +1,28 @@
-import { useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 import CreateToDo from "./CreateToDo";
-import { toDoSelector } from "./atoms";
+import { categoryState, toDoSelector } from "./atoms";
 import ToDo from "./ToDo";
+import React from "react";
 
 function ToDoList() {
-  const [toDo, doing, done] = useRecoilValue(toDoSelector);
+  const toDos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setCategory(event.currentTarget.value);
+  };
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
+      <select value={category} onInput={onInput}>
+        <option value="TO_DO">To Do</option>
+        <option value="DOING">Doing</option>
+        <option value="DONE">Done</option>
+      </select>
       <CreateToDo />
-      <h2>To Do</h2>
-      <ul>
-        {toDo.map((toDo) => (
-          // <ToDo text={toDo.text} category={toDo.category} id={toDo.id} />
-          <ToDo key={toDo.id} {...toDo} />
-          // 이 방식은 위와 같은 내용으로 작동함 ...은 안에 요쇼를 벗기는 역할을 하기 때문
-        ))}
-      </ul>
-      <hr />
-      <h2>Doing</h2>
-      <ul>
-        {doing.map((toDo) => (
-          // <ToDo text={toDo.text} category={toDo.category} id={toDo.id} />
-          <ToDo key={toDo.id} {...toDo} />
-          // 이 방식은 위와 같은 내용으로 작동함 ...은 안에 요쇼를 벗기는 역할을 하기 때문
-        ))}
-      </ul>
-      <hr />
-      <h2>Done</h2>
-      <ul>
-        {done.map((toDo) => (
-          // <ToDo text={toDo.text} category={toDo.category} id={toDo.id} />
-          <ToDo key={toDo.id} {...toDo} />
-          // 이 방식은 위와 같은 내용으로 작동함 ...은 안에 요쇼를 벗기는 역할을 하기 때문
-        ))}
-      </ul>
-      <hr />
+      {toDos?.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
     </div>
   );
 }

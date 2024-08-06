64c365838c8ea3e41c2d342a0c60e795a634bda4
Odyssey <77610388+Odyssey409@users.noreply.github.com>
2024-08-06 17:28:08 +0900

    section#6.18 Enums
    Enumerated type 즉 열거형 타입을 사용하여 여러 파일들에서 카테고리 상태를 문자열 대조로 확인 하는 것이 아닌 열거형 타입의 멤버의 요소로 확인하는 작업을 수행함
    
    열거형은 TypeScript가 제공하는 기능 중 하나입니다.
    
    열거형으로 이름이 있는 상수들의 집합을 정의할 수 있습니다. 열거형을 사용하면 의도를 문서화 하거나 구분되는 사례 집합을 더 쉽게 만들수 있습니다. TypeScript는 숫자와 문자열-기반 열거형을 제공합니다.
    
    숫자 열거형 (Numeric enums)
    다른 언어를 배워 보신 분들이라면 친숙하게 느끼실 수 있는 숫자 열거형에 대해서 먼저 배워보겠습니다. 열거형은 enum 키워드를 사용해 정의할 수 있습니다.
    
    enum Direction {
      Up = 1,
      Down,
      Left,
      Right,
    }
    Try
    위 코드에서 Up이 1 로 초기화된 숫자 열거형을 선언했습니다. 그 지점부터 뒤따르는 멤버들은 자동으로-증가된 값을 갖습니다. 즉 Direction.Up 은 1, Down 은 2, Left 는 3, Right 은 4 을 값으로 가집니다.
    
    문자열 열거형은 유사한 개념이지만 아래 설명된 것과 같이 런타임에서 열거형의 동작이 약간 다릅니다. 문자열 열거형에서 각 멤버들은 문자열 리터럴 또는 다른 문자열 열거형의 멤버로 상수 초기화 해야 합니다.
    
    enum Direction {
      Up = "UP",
      Down = "DOWN",
      Left = "LEFT",
      Right = "RIGHT",
    }
    Try
    문자열 열거형은 숫자 열거형처럼 자동-증가하는 기능은 없지만, “직렬화”를 잘한다는 이점이 있습니다. 다시 말해서 만약 당신이 숫자 열거형을 이용해서 디버깅하고 있고 그 값을 읽어야 한다면, 종종 그 값이 불확실한 경우가 있습니다 - 숫자만으로는 이것이 어떤 의미인지 유의미한 정보를 제공해주지 않기 때문입니다. (역 매핑 을 이용하면 도움이 될지라도 말입니다), 반면 문자열 열거형을 이용하면 코드를 실행할 때, 열거형 멤버에 지정된 이름과는 무관하게 유의미하고 읽기 좋은 값을 이용하여 실행할 수 있습니다.

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { IToDo, toDoState } from "./atoms";
+import { Categories, IToDo, toDoState } from "./atoms";
 import { useSetRecoilState } from "recoil";
 
 function ToDo({ text, category, id }: IToDo) {
@@ -12,7 +12,7 @@ function ToDo({ text, category, id }: IToDo) {
       const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
       //인자로 받아온 toDo의 아이디와 클릭된 투두 목록의 아이디와 일치하는 인데스를 찾는 역할
       const oldToDo = oldToDos[targetIndex];
-      const newToDo = { text, id, category: name as IToDo["category"] };
+      const newToDo = { text, id, category: name as any };
       // name은 어떤 버튼을 눌렀는지에서 받아오는 currentTarget: { name }으로 설정해야 어떤 카테고리로 변경할 것인지 분별가능
       return [
         ...oldToDos.slice(0, targetIndex),
@@ -24,18 +24,18 @@ function ToDo({ text, category, id }: IToDo) {
   return (
     <li>
       <span> {text} </span>
-      {category !== "DOING" && (
-        <button name="DOING" onClick={onClick}>
+      {category !== Categories.DOING && (
+        <button name={Categories.DOING} onClick={onClick}>
           Doing
         </button>
       )}
-      {category !== "TO_DO" && (
-        <button name="TO_DO" onClick={onClick}>
+      {category !== Categories.TO_DO && (
+        <button name={Categories.TO_DO} onClick={onClick}>
           To Do
         </button>
       )}
-      {category !== "DONE" && (
-        <button name="DONE" onClick={onClick}>
+      {category !== Categories.DONE && (
+        <button name={Categories.DONE} onClick={onClick}>
           Done
         </button>
       )}
